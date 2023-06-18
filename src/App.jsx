6dd3be7c5f94acdb91d008ef31fde3d0f22d13b1
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-18 16:19:29 -0300

    will configure the system routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+
 import './App.css'
 
-const App=()=> {
+const App = () => {
   return (
-    <div className='container_app'>
-      <h1>meu blog</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className='container_app'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+          <h1>meu blog</h1>
+        </div>
+      </BrowserRouter>
+    </>
   )
 }
 
