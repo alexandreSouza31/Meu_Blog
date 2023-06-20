e426a0ffb35939b1ca58a647a66eb894742c249a
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-20 20:37:20 -0300

    will create the register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,40 @@
+import "./Register.css";
+
 const Register = () => {
     return (
-        <div>
-            <h2>Register</h2>
+        <div className="register_container">
+            <h1>Cadastre-se agora mesmo!</h1>
+            <form>
+                
+                    <input
+                        type="text"
+                        name="displayName"
+                        required
+                        placeholder="Digite seu nome."
+                    />
+                
+                    <input
+                        type="email"
+                        name="email"
+                        required
+                        placeholder="Digite seu email."
+                    />
+                
+                    <input
+                        type="password"
+                        name="password"
+                        required
+                        placeholder="Digite sua senha."
+                    />
+                
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        required
+                        placeholder="Confirme sua senha."
+                    />
+                <input type="button" value="Cadastrar" />
+            </form>
         </div>
     )
 }
