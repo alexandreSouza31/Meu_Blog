fd9e675b59871f89c2ee36f1765a38a70fa14798
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-20 20:07:00 -0300

    will adjust the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,45 +7,75 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <footer>
-            <p>Nossas redes sociais</p>
-            <ul className="social_list">
-                <li>
-                    <Link
-                        to="https://www.linkedin.com/in/alexandremariano31/"
-                        target="_blank" //abrir em nova guia
-                        rel="noopener noreferrer" /*evitar possíveis ataques
+            <div className="container">
+                <section className="social">
+                    <p>Nossas redes sociais</p>
+                    <ul className="social_list">
+                        <li>
+                            <Link
+                                to="https://www.linkedin.com/in/alexandremariano31/"
+                                target="_blank" //abrir em nova guia
+                                rel="noopener noreferrer" /*evitar possíveis ataques
                         das páginas que eu fizer vínculo*/
-                    >
-                        <FaLinkedinIn />
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="mailto:alexandre31_m@hotmail.com"
-                        target="_blank" rel="noopener noreferrer">
-                        <SiGmail />
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="https://github.com/alexandreSouza31"
-                        target="_blank" rel="noopener noreferrer">
-                        <FaGithub />
-                    </Link>
-                </li>
-                <li>
-                    <Link
-                        to="https://discord.com/channels/@me/831222330206322699"
-                        target="_blank" rel="noopener noreferrer">
-                        <FaDiscord />
-                    </Link>
-                </li>
-            </ul>
-            <p>
-                <span>Meu </span>
-                <span>Blog </span>
-                | &copy; 2023
-            </p>
+                            >
+                                <FaLinkedinIn />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="mailto:alexandre31_m@hotmail.com"
+                                target="_blank" rel="noopener noreferrer">
+                                <SiGmail />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="https://github.com/alexandreSouza31"
+                                target="_blank" rel="noopener noreferrer">
+                                <FaGithub />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="https://discord.com/channels/@me/831222330206322699"
+                                target="_blank" rel="noopener noreferrer">
+                                <FaDiscord />
+                            </Link>
+                        </li>
+                    </ul>
+
+                </section>
+
+                <hr />
+
+                <section className="contact">
+                    <p> Entre em contato conosco</p>
+                    <a> chame no chat clicando aqui!</a>
+                    <p className="phones"> pelos telefones:
+                        <span> áreas metropolitanas 4444-4444</span>
+                        <span> outras regiões 0800-444-4444</span>
+                    </p>
+
+                    <p>Endereço: Av. Frame, 240 - Work, JS - CEP: 99999-999</p>
+                </section>
+
+                <hr />
+
+                <section className="informations">
+                    <a href="/terms">termos de uso Meu Blog</a>
+
+                </section>
+            </div>
+
+            <section className="brand">
+                <p>
+                    <span>Meu Blog </span>
+                    |
+                    <span className="copy">&copy;</span>
+                     2023
+                </p>
+            </section>
+
         </footer>
     )
 }
