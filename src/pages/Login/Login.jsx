79dbbe600a10e653072b0336d84151bcd3b3ba68
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-20 22:20:52 -0300

    will add the login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,26 @@
+import "./Login.css";
+
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="login_container">
+            <h1>Faça Login e aproveite!</h1>
+            <form>
+                    <input
+                        type="email"
+                        name="email"
+                        required
+                        placeholder="Digite seu email."
+                    />
+                
+                    <input
+                        type="password"
+                        name="password"
+                        required
+                        placeholder="Digite sua senha."
+                    />
+                
+                <input type="button" value="Entrar" />
+            </form>
         </div>
     )
 }
