7a3b4516989485b7b29476a5d2c3b3056775e9e0
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-20 23:38:02 -0300

    will add links to login and register

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
     return (
         <div className="container_home">
+            <h1>Bem-vindo(a) ao meu blog!</h1>
             <h1>Crie, compartilhe, interaja!</h1>
+            <div className="links">
+            <Link to="/login"><h2>Faça login,</h2></Link>
+            ou
+            <Link to="/register"><h2>Cadastre-se!</h2></Link>
+
+            </div>
+
         </div>
     )
 }
