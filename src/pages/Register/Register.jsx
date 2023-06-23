65f5a19be21fe6b0a8032ae557b1cf22cb1f6097
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-06-23 00:17:40 -0300

    will create the form states

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,39 +1,73 @@
+import { useState } from "react";
+
 import "./Register.css";
 
 const Register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError("");//começa vazio
+        //agr vou formar os usuários baseado nos inputs:
+        const user = {
+            name,
+            email,
+            password,
+            confirmPassword
+        }
+
+        if (password !== confirmPassword) {
+            setError("As senhas precisam ser iguais!");
+            return;
+        }console.log(user)
+    }
+
     return (
         <div className="register_container">
             <h1>Cadastre-se agora mesmo!</h1>
-            <form>
-                
+            
+            <form onSubmit={handleSubmit}>
                     <input
                         type="text"
-                        name="displayName"
+                        name="name"
                         required
-                        placeholder="Digite seu nome."
+                    placeholder="Digite seu nome."
+                    value={name}
+                    onChange={(e)=>setName(e.target.value)}
                     />
                 
                     <input
                         type="email"
                         name="email"
                         required
-                        placeholder="Digite seu email."
+                    placeholder="Digite seu email."
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
                     />
                 
                     <input
                         type="password"
                         name="password"
                         required
-                        placeholder="Digite sua senha."
+                    placeholder="Digite sua senha."
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
                     />
                 
                     <input
                         type="password"
                         name="confirmPassword"
                         required
-                        placeholder="Confirme sua senha."
+                    placeholder="Confirme sua senha."
+                    value={confirmPassword}
+                    onChange={(e)=>setConfirmPassword(e.target.value)}
                     />
-                <input type="button" value="Cadastrar" />
+                <input type="submit" value="Cadastrar" />
             </form>
         </div>
     )
