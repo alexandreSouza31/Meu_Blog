460a88ddbe96004b9af58f2724f9b40278abe6d5
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-12 13:39:13 -0300

    will show specific links for authenticated user

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -3,10 +3,9 @@ import { createContext, useContext } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children, value }) => {
-    return(
-    <AuthContext.Provider value={value}>
+    return <AuthContext.Provider value={value}>
         {children}
-    </AuthContext.Provider>)
+    </AuthContext.Provider>
 }
 
 export const useAuthValue = () => {
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,22 +1,49 @@
 import { NavLink } from "react-router-dom";
+import { useAuthValue } from "../context/AuthContext";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 import "./Navbar.css";
+
 const Navbar = () => {
+    const { user } = useAuthValue();//chamo o contexto
+    
+
     return (
         <nav>
-            <NavLink to="/" end className={({ isActive }) => (isActive ? ".active" : "")}>
+            <NavLink to="/" end
+                className={({ isActive }) => (isActive ? ".active" : "")}
+>
                 <img src="/MeuBlog.png" alt="logo Meu Blog" />
             </NavLink>
             <ul>
                 <li>
-                    <NavLink to="/" end >Home</NavLink>
+                    <NavLink to="/" end
+                         >Home</NavLink>
                 </li>
-                <li>
-                    <NavLink to="/login" end >Entrar</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register" end >Cadastrar</NavLink>
-                </li>
+                {user !==undefined && (
+                    <>
+                        <li>
+                            <NavLink to="/login" end
+                            >Entrar</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" end
+                            >Cadastrar</NavLink>
+                        </li>
+                    </>
+                )}
+                {user === undefined && (
+                     <>
+                     <li>
+                         <NavLink to="/dashboard" end
+                         >Dashboard</NavLink>
+                     </li>
+                     <li>
+                         <NavLink to="/posts/create" end
+                         >Post</NavLink>
+                     </li>
+                 </>
+                )}
                 <li>
                     <NavLink to="/about" >Sobre</NavLink>
                 </li>
