6de69c97ac8a8bc9b41fc69191183f2b6d058f4f
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-12 22:46:09 -0300

    will styles the about component

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,7 +1,16 @@
+import "./About.css"
+
 const About = () => {
     return (
-        <div>
+        <div className="container_about">
             <h2>About</h2>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
         </div>
     )
 }
