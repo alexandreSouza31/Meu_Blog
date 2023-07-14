f63fb5b9396bcec80ddaba6194c8be6485ca1e33
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-14 12:03:39 -0300

    will create the post link

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -8,6 +8,7 @@ const About = () => {
             <p>Este app usa React.js no Front-end, e no back-end, o Firebase.</p>
             <p>Aqui o usuário pode realizar um cadastro, logar na aplicação, e com isso realizar posts, comentar e curtir.</p>
             <p>Viva essa experiência!</p>
+            <Link to="/posts/create">Crie seu post agora mesmo!</Link>
             
         </div>
     )
