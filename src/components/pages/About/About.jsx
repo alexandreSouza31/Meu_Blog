37b90836ee882f9977f661568c54342f02e6c2d7
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-14 12:02:57 -0300

    will change the text in about

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,16 +1,14 @@
+import { Link } from "react-router-dom";
 import "./About.css"
 
 const About = () => {
     return (
         <div className="container_about">
-            <h2>About</h2>
-            <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
-            <br />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iste dolores maxime at repudiandae ipsam labore ea, illo esse, saepe culpa necessitatibus aliquam vitae magni quia dolor corrupti, voluptate debitis!</p>
+            <h2>Sobre o Meu Blog</h2>
+            <p>Este app usa React.js no Front-end, e no back-end, o Firebase.</p>
+            <p>Aqui o usuário pode realizar um cadastro, logar na aplicação, e com isso realizar posts, comentar e curtir.</p>
+            <p>Viva essa experiência!</p>
+            
         </div>
     )
 }
