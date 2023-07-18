f69792dfa9b6ba264a6dcebcf2b0961f6672b780
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-18 13:42:41 -0300

    will adjust the styles in the page

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -4,7 +4,7 @@ import "./About.css"
 const About = () => {
     return (
         <div className="container_about">
-            <h2>Sobre o Meu Blog</h2>
+            <h1>Sobre o Meu Blog</h1>
             <p>Este app usa React.js no Front-end, e no back-end, o Firebase.</p>
             <p>Aqui o usuário pode realizar um cadastro, logar na aplicação, e com isso realizar posts, comentar e curtir.</p>
             <p>Viva essa experiência!</p>
