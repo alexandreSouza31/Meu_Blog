8668e0e06faf8949c8dc4c8959d20a15a3f5b922
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-18 13:48:22 -0300

    will adjust the styles in the page

--- src/components/pages/Dashboard/Dashboard.jsx
+++ src/components/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,10 @@
 
+import  "./Dashboard.css"
+
 export const Dashboard = () => {
     return (
-        <>
+        <div className="container_dashboard">
             <p>esse é o conteúdo do dashboard!</p>
-        </>
+        </div>
     )
 }
