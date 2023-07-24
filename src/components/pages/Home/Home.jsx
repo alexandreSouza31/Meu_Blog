28b2c244a4e11f28c80e26c534f56a0fdfbdfdf6
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-24 09:37:07 -0300

    will add the posts in the home page

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-import "./Home.css";
 import { Link } from "react-router-dom";
+import { useFetchDocument } from "../../../hooks/useFetchDocument";
+import { FaSearch, FaSmileBeam, FaUser } from "react-icons/fa"
+
+import "./Home.css";
+import "./DetailPost.css";
 
 const Home = () => {
 
     const [query, setQuery] = useState("");
-    const [posts] = useState([]);
+    const { documents: posts, loading } = useFetchDocument("posts");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -13,17 +17,40 @@ const Home = () => {
 
     return (
         <div className="container_home">
-            <h1>Veja os posts mais recentes</h1>
             <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="busque posts por tags"
-                    onChange={(e) => setQuery(e.target.value)} />
-                <input type="button" value="Pesquisar" />
+                <div className="container_search">
+
+                    <input
+                        type="text"
+                        placeholder="busque posts por tags"
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                    {/* <input type="button" value="Pesquisar" /> */}
+                    <button><FaSearch /></button>
+                </div>
+                <h2>Veja os posts mais recentes</h2>
             </form>
+            <hr />
 
             <div className="posts_container">
-                <h2>posts...</h2>
+                {loading && <p>Carregando</p>}
+                {posts && posts.map((post) => (
+                    <div key={query} className="card_post">
+                        <p className="container_user">
+                            <span >< FaUser /></span>
+                            <p className="user">{post.createdBy}</p>
+                        </p>
+                        <div className="post_details">
+                            <h3>{post.title}</h3>
+                            <h4>{post.body}</h4>
+                            <p>{post.tagsArray}</p>
+                            <img src={post.image} alt="" />
+                        </div>
+
+                    </div>
+                ))}
+
+                <p className="end_posts">É só isso por enquanto.<FaSmileBeam /></p>
                 {posts && posts.length === 0 && (
                     <div className="no_posts">
                         <p>Não foi encontrado nenhum post</p>
