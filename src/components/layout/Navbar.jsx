2dec8bd251b271786c54f4fa2b807f9e38799d67
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-07-31 13:46:47 -0300

    will add the hamburguer menu

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,84 +3,99 @@ import { useAuthValue } from "../context/AuthContext";
 import { useAuthentication } from "../../hooks/useAuthentication";
 
 import "./Navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
-    const userIsLogged = useAuthValue(); //vem pelo contexto de autenticação
-    const { logout } = useAuthentication();//função logout está no hook useauthentication. Só importo aqui.
+    const userIsLogged = useAuthValue();
+    const { logout } = useAuthentication();
+    const [isOpen, setIsOpen] = useState(false);
 
-    // Function to render navigation items based on the user's authentication status
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     const renderNavigationItems = () => {
         if (userIsLogged) {
             return (
                 <>
-                    <li>
-                        <NavLink to="/home" end>Home</NavLink>
-                    </li>
-                    <span className="pipe_link"> |</span>
-                    <li>
-                        <NavLink to="/dashboard" end>Dashboard</NavLink>
-                    </li>
-                    <span className="pipe_link"> |</span>
-                    <li>
-                        <NavLink to="/posts/create" end>Post</NavLink>
-                    </li>
-                    <span className="pipe_link"> |</span>
-                    <li>
-                        <NavLink to="/about">Sobre</NavLink>
-                    </li>
-                    <span className="pipe_link"> |</span>
-
+                    {isOpen && (
+                        <ul className={`menu ${isOpen ? "open" : ""}`}>
+                            <li>
+                                <NavLink to="/home" end>Home</NavLink>
+                            </li>
+                            <span className="pipe_link"> |</span>
+                            <li>
+                                <NavLink to="/dashboard" end>Dashboard</NavLink>
+                            </li>
+                            <span className="pipe_link"> |</span>
+                            <li>
+                                <NavLink to="/posts/create" end>Post</NavLink>
+                            </li>
+                            <span className="pipe_link"> |</span>
+                            <li>
+                                <NavLink to="/about">Sobre</NavLink>
+                            </li>
+                            <span className="pipe_link"> |</span>
+                            <li>
+                                <button className="logout_btn" onClick={logout}>Sair</button>
+                            </li>
+                        </ul>
+                    )}
                 </>
             );
         } else {
             return (
                 <>
-                    <li>
-                        <NavLink to="/" end>Home</NavLink>
-                    </li>
-                    <span className="pipe_link"> |</span>
-                    <li>
-                        <NavLink to="/login" end>Entrar</NavLink>
-                    </li>
-                    <span className="pipe_link"> |</span>
-                    <li>
-                        <NavLink to="/register" end>Cadastrar</NavLink>
-                    </li>
-                    <span className="pipe_link"> |</span>
-                    <li>
-                        <NavLink to="/about">Sobre</NavLink>
-                    </li>
+                    {isOpen && (
+                        <ul className={`menu ${isOpen ? "open" : ""}`}>
+                            <li>
+                                <NavLink to="/" end>Início</NavLink>
+                            </li>
+                            <span className="pipe_link"> |</span>
+                            <li>
+                                <NavLink to="/login" end>Entrar</NavLink>
+                            </li>
+                            <span className="pipe_link"> |</span>
+                            <li>
+                                <NavLink to="/register" end>Cadastrar</NavLink>
+                            </li>
+                            <span className="pipe_link"> |</span>
+                            <li>
+                                <NavLink to="/about">Sobre</NavLink>
+                            </li>
+                        </ul>
+                    )}
                 </>
             );
         }
     };
 
     return (
-        <nav>
-            <NavLink to="/" end className={({ isActive }) => (isActive ? ".active" : "")}>
-                <img src="/MeuBlog.png" alt="logo Meu Blog" />
+        <>
+            <nav>
+                <NavLink to="/" end className={({ isActive }) => (isActive ? ".active" : "")}>
+                    <img src="/MeuBlog.png" alt="logo Meu Blog" />
+                    {userIsLogged && (
+                        <p className="nav_user_small">
+                            Olá, {userIsLogged.displayName}!
+                        </p>
+                    )}
+                </NavLink>
+
                 {userIsLogged && (
-                    <p className="nav_user_small">
+                    <p className="nav_user">
                         Olá, {userIsLogged.displayName}!
                     </p>
-                )}
-            </NavLink>
 
-            {userIsLogged && (
-                <p className="nav_user">
-                    Olá, {userIsLogged.displayName}!
-                </p>
-            )}
-
-            <ul>
-                {renderNavigationItems()}
-                {userIsLogged && (
-                    <li>
-                        <button className="logout_btn" onClick={logout}>Sair</button>
-                    </li>
                 )}
-            </ul>
-        </nav>
+                <button className={`hamburguer ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </button>
+            </nav>
+            {renderNavigationItems()} {/* Renderiza os itens de navegação */}
+        </>
     );
 };
 
