b5b6cede5d70ecd86093f0d2820d9c4be87882bd
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-08-05 14:06:44 -0300

    will adjust the input component

---
+++ src/components/elements/CustomInput.jsx
@@ -0,0 +1,18 @@
+// CustomInput.js
+
+const CustomInput = ({ type, name, required, placeholder, value, onChange }) => {
+  return (
+    <div className="input-wrapper">
+      <input
+        type={type}
+        name={name}
+        required={required}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default CustomInput;
